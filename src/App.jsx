c2deb28f09484c3205a56c05686e6201710422b7
HeroicSam <cyuen10@gmail.com>
2023-03-27 20:20:16 -0400

    feat: significant progress on gsap transition animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import gsap from "gsap"
 import { Observer } from "gsap/Observer";
 
@@ -12,11 +12,16 @@ function App() {
   gsap.registerPlugin(Observer);
 
   const isMobile = useWidthBreakpointReached("md")
+  const sections = useRef([]);
+  const textSections = useRef([]);
+
+  let currentIndex = 0,
+      wrap,
+      animating;
 
   useLayoutEffect(() => {
 
     const t1 = gsap.timeline();
-    const t2 = gsap.timeline();
 
     t1.from(".ease", {
       y: 400,
@@ -38,12 +43,48 @@ function App() {
     })
     
     Observer.create({
-      target: window,
       type: "wheel, touch, scroll, pointer",
-      onChange: (self) => console.log(self.deltaY)
+      wheelSpeed: -1,
+      onDown: () => !animating && goToSection(currentIndex - 1, -1),
+      onUp: () => !animating && goToSection(currentIndex + 1, 1),
+      tolerance: 130,
     })
 
-  }, [])
+    wrap = gsap.utils.wrap(0, sections.length)
+
+    console.log(sections)
+
+  }, [])  
+
+  function goToSection(index, direction) {
+    console.log("fn", sections)
+    index = wrap(index)
+    animating = true
+    let fromTop = direction === 1,
+        dFactor = fromTop ? -1 : 1,
+        t2 = gsap.timeline({
+          defaults: { duration: 1.25, ease: "power1.inOut" },
+          onComplete: () => animating = false
+        })
+    
+    if (currentIndex >= 0) {
+      console.log('if running')
+      gsap.set(sections.current[0], { zIndex: 0, });
+      t2.to(textSections.current[0], { opacity: 0, duration: 1 })
+        .set(sections.current[0], { autoAlpha: 0, duration: 1  })
+    }
+    gsap.to(sections.current[1], { autoAlpha: 1, zIndex: 1, duration: 2, delay: 1 })
+    gsap.to(".tColor", {
+      backdropFilter: "saturate(180%)",
+      duration: 2,
+      delay: 1
+    })
+    gsap.to(".tColor", {
+      backdropFilter: "blur(40px)",
+      duration: 2,
+      delay: 1
+    })
+  }
 
   return (
     <div className="App">
@@ -51,15 +92,16 @@ function App() {
         <NavBar isMobile={isMobile} />
         <div className="flex w-full h-full md:py-20 md:px-16 py-16 px-8">
           <div className="tColor w-full h-full sm:border">
-            <secion className="flex flex-col h-full justify-end text-white pb-14 pl-14">
-              <p className="cooper lg:text-9xl sm:text-8xl text-5xl">I'm a</p>
-              <p className="cooper lg:text-9xl sm:text-8xl text-5xl">Frontend</p>
-              <p className="cooper lg:text-9xl sm:text-8xl text-5xl">Engineer</p>
-            </secion>
-            {/* <section className="flex flex-col h-full text-white py-14 px-14">
-              <h1 className="text-9xl">Southpole</h1>
-            </section> */}
-            {/* <section className="flex flex-col h-full w-1/3 text-white pt-28 px-14">
+
+            <div ref={el => sections.current[0] = el} className="homepage fixed flex flex-col h-[85%] justify-end text-white mb-14 pl-14">
+              <div ref={el => textSections.current[0] = el} className="textSectionOne">
+                <p className="cooper lg:text-9xl sm:text-8xl text-5xl">I'm a</p>
+                <p className="cooper lg:text-9xl sm:text-8xl text-5xl">Frontend</p>
+                <p className="cooper lg:text-9xl sm:text-8xl text-5xl">Engineer</p>
+              </div>
+            </div>
+
+            <div ref={el => sections.current[1] = el} className="flex fixed flex-col h-[85%] w-1/3 text-white pt-28 px-14 invisible">
               <h1 className="text-9xl mb-4 projects">Projects</h1>
               <div
                 className="accordion flex justify-start border-t-4 h-24 pt-2 text-2xl hover:pointer overflow-hidden gap-x-2"
@@ -80,7 +122,8 @@ function App() {
               <div className="flex items-center border-t-4 h-24 pt-2 border-b-4 text-2xl">Algorithm Visualizer</div>
               <div className="w-full h-full pt-8">
               </div>
-            </section> */}
+            </div>
+
           </div>
         </div>
         {/* <div className="scrollTextContainer w-full h-max-content flex justify-center mt-8">
