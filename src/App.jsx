09d0b43d39f93068bbd1c6798095da7d27ca1286
HeroicSam <cyuen10@gmail.com>
2023-03-29 20:40:50 -0400

    feat: gsap animation for project section

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,14 @@ function App() {
   const isMobile = useWidthBreakpointReached("md")
 
   const sections = useRef([]);
+  
   const textSections = useRef([]);
+
+  const projectSection = useRef([]);
+  const projectTitle = useRef([]);
+  const projectStack = useRef([]);
+  const projectLink = useRef([]);
+  const projectBg = useRef([]);
 
   let currentIndex = -1,
       wrap,
@@ -32,9 +39,6 @@ function App() {
       stagger: {
         amount: 0.4,
       },
-    }).from(".tColor", {
-      opacity: 0,
-      duration: .2
     }).to('.scrollText', {
       y: '8px',
       duration: 1,
@@ -106,14 +110,37 @@ function App() {
       gsap.to(sections.current[index], { autoAlpha: 1, zIndex: 1, duration: 1, delay: .5 })
     }
     currentIndex = index;
+
+    gsap.set(projectBg.current[0], {
+      x: -projectBg.current[0].offsetWidth
+    })
+  }
+
+
+
+  function handleHover(index, hovering) {
+
+    if (hovering){
+      gsap.to(projectBg.current[0], {
+        x: 0,
+        duration: 0.2,
+        ease: "power4.easeOut"
+      })
+    }
+    if (!hovering){
+      gsap.to(projectBg.current[0], {
+        x: -projectBg.current[0].offsetWidth,
+        duration: 0.2
+      })
+    }
   }
 
   return (
     <div className="App">
-      <div className='bg-transparent z-50 absolute w-full h-full'>
+      <div className='bg-transparent z-50 absolute w-full h-full overflow-y-hidden'>
         <NavBar isMobile={isMobile} />
         <div className="flex w-full h-[90%] md:h-[100%] md:h-full md:py-20 md:px-16 md:pb-32 mt-16 md:mt-0 ">
-          <div className="tColor w-full h-full sm:border overflow-y-hidden mt-8">
+          <div className="tColor w-full h-full sm:border overflow-hidden mt-8">
 
             <div ref={el => sections.current[0] = el} className="homepage fixed flex flex-col h-full md:h-[93%] w-full justify-end text-white md:mb-14 px-4 md:px-16  invisible">
               <div ref={el => textSections.current[0] = el} className="textSectionOne">
@@ -125,40 +152,126 @@ function App() {
               </div>
             </div>
 
-            <div ref={el => sections.current[1] = el} className="flex fixed flex-col h-full w-full text-white pt-8 md:pt-16 px-4 md:px-14 invisible">
+            <div ref={el => sections.current[1] = el} className="flex fixed flex-col h-full w-full text-white pt-8 md:pt-0 px-4 md:px-0 invisible">
               <div ref={el => textSections.current[1] = el} >
-                <h1 className="lg:text-8xl text-4xl mb-4 projects">A few things I've worked on...</h1>
-                <div className="w-1/3">
-                  <div
-                    className="accordion flex justify-start border-t-4 h-28 pt-2 text-2xl hover:pointer overflow-hidden gap-x-2"
-                    onMouseEnter={() => handleAccordion()}
-                    onMouseLeave={() => handleAccordion()}
-                  >
-                    <div className="arrow">
-                      <div>
-                        Hotswaps
-                      </div>
-                      <div className="text-sm">
-                        React / TailwindCSS / PostgreSQL / Express / AWS
-                      </div>
+
+                <div
+                  className="accordion flex justify-between items-center border-t-2 h-36 px-8 text-2xl hover:pointer overflow-hidden gap-x-2"
+                  onMouseEnter={() => handleHover(0, true)}
+                  onMouseLeave={() => handleHover(0, false)}
+                  ref={el => projectSection.current[0] = el} 
+                >
+                  <div ref={el => projectBg.current[0] = el}  className="absolute h-36 w-full bg-white ml-[-2rem]" />
+                  <div className="flex flex-col justify-center">
+                    <div
+                      className="text-6xl"
+                      ref={el => projectTitle.current[0] = el} 
+                    >
+                      Spotlist
+                    </div>
+                    <div 
+                      className="text-sm"
+                      ref={el => projectStack.current[0] = el} 
+                    >
+                      React Native / Django 
                     </div>
                   </div>
-                  <div className="flex items-center border-t-4 h-24 pt-2 text-2xl">AudioAnalyzer</div>
-                  <div className="flex items-center border-t-4 h-24 pt-2 text-2xl">Client to Server Network Visualization</div>
-                  <div className="flex items-center border-t-4 h-24 pt-2 border-b-4 text-2xl">Algorithm Visualizer</div>
-                  <div className="w-full h-full pt-8">
+                  <a target="_blank" href="https://www.github.com">
+                    <div 
+                      className="text-lg underline"
+                      ref={el => projectLink.current[0] = el} 
+                    >
+                      website
+                    </div>
+                  </a>
+                </div>
+
+                <div
+                  className="accordion flex justify-between items-center border-t-2 h-36 pt-2 px-8 text-2xl hover:pointer overflow-hidden gap-x-2"
+                  onMouseEnter={() => handleHover()}
+                  onMouseLeave={() => handleHover()}
+                >
+                  <div className="flex flex-col justify-center">
+                    <div className="text-6xl">
+                      Hotswaps
+                    </div>
+                    <div className="text-sm">
+                      React / TailwindCSS / PostgreSQL / Express / AWS
+                    </div>
                   </div>
+                  <a target="_blank" href="https://www.github.com">
+                    <div className="text-lg underline">
+                      website
+                    </div>
+                  </a>
+                </div>
+                <div
+                  className="accordion flex justify-between items-center border-t-2 h-36 pt-2 px-8 text-2xl hover:pointer overflow-hidden gap-x-2"
+                  onMouseEnter={() => handleHover()}
+                  onMouseLeave={() => handleHover()}
+                >
+                  <div className="flex flex-col justify-center">
+                    <div className="text-6xl">
+                      AudioAnalyzer
+                    </div>
+                    <div className="text-sm">
+                      React / TailwindCSS / PostgreSQL / Express / AWS
+                    </div>
+                  </div>
+                  <a target="_blank" href="https://www.github.com">
+                    <div className="text-lg underline">
+                      github
+                    </div>
+                  </a>
+                </div>
+                <div
+                  className="accordion flex justify-between items-center border-t-2 h-36 pt-2 px-8 text-2xl hover:pointer overflow-hidden gap-x-2"
+                  onMouseEnter={() => handleHover()}
+                  onMouseLeave={() => handleHover()}
+                >
+                  <div className="flex flex-col justify-center">
+                    <div className="text-6xl">
+                      Client-Server Network Visualization
+                    </div>
+                    <div className="text-sm">
+                      React / TailwindCSS / PostgreSQL / Express / AWS
+                    </div>
+                  </div>
+                  <a target="_blank" href="https://www.github.com">
+                    <div className="text-lg underline">
+                      github
+                    </div>
+                  </a>
+                </div>
+                <div
+                  className="accordion flex justify-between items-center border-t-2 border-b-2 h-36 pt-2 px-8 text-2xl hover:pointer overflow-hidden gap-x-2"
+                  onMouseEnter={() => handleHover()}
+                  onMouseLeave={() => handleHover()}
+                >
+                  <div className="flex flex-col justify-center">
+                    <div className="text-6xl">
+                      Algorithm Visualizer
+                    </div>
+                    <div className="text-sm">
+                      React / TailwindCSS / PostgreSQL / Express / AWS
+                    </div>
+                  </div>
+                  <a target="_blank" href="https://www.github.com">
+                    <div className="text-lg underline">
+                      github
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
 
-            <div ref={el => sections.current[2] = el} className="flex fixed flex-col h-full text-white pt-28 px-14 invisible">
+            <div ref={el => sections.current[2] = el} className="flex fixed flex-col h-full w-full text-white pt-28 px-14 invisible">
             <div ref={el => textSections.current[2] = el} >
                 About Me
               </div>
             </div>
 
-            <div ref={el => sections.current[3] = el} className="flex fixed flex-col h-full text-white pt-28 px-14 invisible">
+            <div ref={el => sections.current[3] = el} className="flex fixed flex-col h-full w-full text-white pt-28 px-14 invisible">
             <div ref={el => textSections.current[3] = el} >
                 Contact Me
               </div>
@@ -182,12 +295,17 @@ export default App;
 
 
 // ideas
+// 1. Navigation in NavBar -> dot dot sectionName dot dot
+//  - on mobile this could be on the bottom?
+// 2. 
+
 // https://codepen.io/GreenSock/pen/XWzRraJ
 // add custom mouse circle, goes well with balls
-
 // https://codepen.io/GreenSock/pen/oNYXzYB
 
-// To-Dotttt66ttttttttttttttttttttttttttttttttttttttttttt
-// 1. Make animation times faster
-// 2. Disable scroll until first transition is finished
+// To-Do
+// 2. Disable scroll until first transition is finished -- May need if implementing loading page
 // 3. Something wrong going on with mobile landscape and canvas(?), may need resizing?
+
+// Done
+// 1. Make animation times faster -- DONE
